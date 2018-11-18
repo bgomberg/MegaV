ad40e5ccba1c1f9ff0f78fc8a4b1ba82f0ba71fa
Brian Gomberg <briangomberg@gmail.com>
2018-11-18 19:49:40 +0000

    Style cleanup

--- experiments/basic/basic.sv
+++ experiments/basic/basic.sv
@@ -1,36 +1,37 @@
 /*
  * Basic module which sets the result to the sum of a and b on the rising edge of the clock
  */
-module basic(clk, a, b, result);
+module basic #(
+    parameter DATA_WIDTH = 3
+)(
+    input wire clk,
+    input wire [DATA_WIDTH-1:0] a,
+    input wire [DATA_WIDTH-1:0] b,
+    output wire [DATA_WIDTH-1:0] result
+);
 
-input clk;
-input [2:0] a;
-input [2:0] b;
-output [2:0] result;
+    /*
+     * Adder logic
+     */
+    initial result = 0;
+    always @(posedge clk) begin
+    	result <= a + b;
+    end
 
-reg	[2:0] result;
-
-initial result = 0;
-always @(posedge clk) begin
-	result <= a + b;
-end
-
-/*
- * Formal properties
- */
+    /*
+     * Formal properties
+     */
 `ifdef FORMAL
+    initial	f_past_valid = 1'b0;
+    always @(posedge clk) begin
+    	f_past_valid <= 1'b1;
+    end
 
-initial	f_past_valid = 1'b0;
-always @(posedge clk) begin
-	f_past_valid <= 1'b1;
-end
-
-always @(posedge clk) begin
-	if (f_past_valid) begin
-		assert(result == $past(a) + $past(b));
-	end
-end
-
+    always @(posedge clk) begin
+    	if (f_past_valid) begin
+    		assert(result == $past(a) + $past(b));
+    	end
+    end
 `endif
 
 endmodule
