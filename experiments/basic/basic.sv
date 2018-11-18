aeac87072cb87f4dc7141df144f225610ed6d897
Brian Gomberg <briangomberg@gmail.com>
2018-11-18 18:07:58 +0000

    Added SBY file and make target

--- experiments/basic/basic.sv
+++ experiments/basic/basic.sv
@@ -1,3 +1,6 @@
+/*
+ * Basic module which sets the o_result to the sum of i_a and i_b on the rising edge of the clock
+ */
 module basic(i_clk, i_a, i_b, o_result);
 
 input i_clk;
@@ -5,12 +8,29 @@ input [2:0] i_a;
 input [2:0] i_b;
 output [2:0] o_result;
 
-reg	[2:0] out;
+reg	[2:0] o_result;
 
-initial out = 0;
-always @(posedge i_clk)
-	out <= i_a + i_b;
+initial o_result = 0;
+always @(posedge i_clk) begin
+	o_result <= i_a + i_b;
+end
 
-assign o_result = out;
+/*
+ * Formal properties
+ */
+`ifdef FORMAL
+
+initial	f_past_valid = 1'b0;
+always @(posedge i_clk) begin
+	f_past_valid <= 1'b1;
+end
+
+always @(posedge i_clk) begin
+	if (f_past_valid) begin
+		assert(o_result == $past(i_a) + $past(i_b));
+	end
+end
+
+`endif
 
 endmodule
