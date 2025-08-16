585f92ec40dc561d6c58b11de23106f04ee09b88
Brian Gomberg <briangomberg@gmail.com>
2025-08-16 05:01:51 -0700

    Refactor shifter

--- core/verilog/shifter.sv
+++ core/verilog/shifter.sv
@@ -1,6 +1,8 @@
 `ifndef __SHIFTER_SV__
 `define __SHIFTER_SV__
 
+`include "cells/mux2.sv"
+
 function [31:0] reverse_bit_order(input [31:0] data);
 integer i;
 begin
@@ -22,13 +24,54 @@ module shifter(
 );
 
     wire shift_in_bit = is_right_arithmetic & in[31];
-    wire [31:0] shift_in = is_right ? reverse_bit_order(in) : in;
-    wire [31:0] shift_int_1 = amount[4] ? {shift_in[15:0], {16{shift_in_bit}}} : shift_in;
-    wire [31:0] shift_int_2 = amount[3] ? {shift_int_1[23:0], {8{shift_in_bit}}} : shift_int_1;
-    wire [31:0] shift_int_3 = amount[2] ? {shift_int_2[27:0], {4{shift_in_bit}}} : shift_int_2;
-    wire [31:0] shift_int_4 = amount[1] ? {shift_int_3[29:0], {2{shift_in_bit}}} : shift_int_3;
-    wire [31:0] shift_int_5 = amount[0] ? {shift_int_4[30:0], shift_in_bit} : shift_int_4;
-    assign out = is_right ? reverse_bit_order(shift_int_5) : shift_int_5;
+    wire [31:0] shift_in;
+    mux2 #(.BITS(32)) shift_in_mux(
+        .a(in),
+        .b(reverse_bit_order(in)),
+        .select(is_right),
+        .out(shift_in)
+    );
+    wire [31:0] shift_int_1;
+    mux2 #(.BITS(32)) shift_int_1_mux(
+        .a(shift_in),
+        .b({shift_in[15:0], {16{shift_in_bit}}}),
+        .select(amount[4]),
+        .out(shift_int_1)
+    );
+    wire [31:0] shift_int_2;
+    mux2 #(.BITS(32)) shift_int_2_mux(
+        .a(shift_int_1),
+        .b({shift_int_1[23:0], {8{shift_in_bit}}}),
+        .select(amount[3]),
+        .out(shift_int_2)
+    );
+    wire [31:0] shift_int_3;
+    mux2 #(.BITS(32)) shift_int_3_mux(
+        .a(shift_int_2),
+        .b({shift_int_2[27:0], {4{shift_in_bit}}}),
+        .select(amount[2]),
+        .out(shift_int_3)
+    );
+    wire [31:0] shift_int_4;
+    mux2 #(.BITS(32)) shift_int_4_mux(
+        .a(shift_int_3),
+        .b({shift_int_3[29:0], {2{shift_in_bit}}}),
+        .select(amount[1]),
+        .out(shift_int_4)
+    );
+    wire [31:0] shift_int_5;
+    mux2 #(.BITS(32)) shift_int_5_mux(
+        .a(shift_int_4),
+        .b({shift_int_4[30:0], shift_in_bit}),
+        .select(amount[0]),
+        .out(shift_int_5)
+    );
+    mux2 #(.BITS(32)) out_mux(
+        .a(shift_int_5),
+        .b(reverse_bit_order(shift_int_5)),
+        .select(is_right),
+        .out(out)
+    );
 
 `ifdef FORMAL
     /* Validate logic */
